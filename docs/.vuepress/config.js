5b06eba4ed371408e20fab760ed6567eb7e7e9e3
tangtanglove <l>
2020-11-05 20:02:53 +0800

    提交代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,6 +62,8 @@ module.exports = ({
                     children: [
                         '',
                         'installation',
+                        'container',
+                        'card',
                         'table',
                         'form',
                         'show'
